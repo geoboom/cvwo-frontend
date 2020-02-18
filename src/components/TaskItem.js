c01ec74d1aa560d7b4b17ddd06e8f6ac657dd7c5
geoboom <leegeorgie52@gmail.com>
2020-02-18 23:20:03 +0800

    typo

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -12,7 +12,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
-import TextField from '@material-ui/core/Textfield';
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Chip from '@material-ui/core/Chip';
 import Modal from '@material-ui/core/Modal';
